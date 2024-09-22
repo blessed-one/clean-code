b5cc08c3326e4e4fe4f4e31206c405bd82ad288e
blessed-one <shakirovdanis05@gmail.com>
2024-09-23 00:07:14 +0400

    added interfaces for renderer and parser

---
+++ MarkdownContract/MarkdownContract/IParse.cs
@@ -0,0 +1,17 @@
+﻿namespace MarkdownContract
+{
+    public interface IParse
+    {
+        /// <summary>
+        /// Ищет следющий тег
+        /// </summary>
+        /// <returns>true, если найден следущий тэг, и false, если не найден</returns>
+        public bool FindTag();
+
+        /// <summary>
+        /// Конвертирует MD тег в HTML
+        /// </summary>
+        /// <returns>HTML тег, соответствующий </returns>
+        public string ConvertTag();
+    }
+}
---
+++ MarkdownContract/MarkdownContract/IRender.cs
@@ -0,0 +1,12 @@
+﻿namespace MarkdownContract
+{
+    public interface IRender
+    {
+        /// <summary>
+        /// Конвертирует текст с Markdown разметкой в HTML
+        /// </summary>
+        /// <param name="text">Текст с md разметкой</param>
+        /// <returns>Текст с html разметкой</returns>
+        public string Render(string text);
+    }
+}
