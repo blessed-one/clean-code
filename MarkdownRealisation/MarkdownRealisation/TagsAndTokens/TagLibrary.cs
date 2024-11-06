cc31ce9263dd5fbbe57e00359548b28a8a270d31
blessed-one <shakirovdanis05@gmail.com>
2024-11-07 00:11:25 +0300

    изменил библиотеку тегов под новые типы тегов

--- MarkdownRealisation/MarkdownRealisation/TagsAndTokens/TagLibrary.cs
+++ MarkdownRealisation/MarkdownRealisation/TagsAndTokens/TagLibrary.cs
@@ -5,16 +5,16 @@ namespace MarkdownRealisation.TagsAndTokens
     public static class TagLibrary
     {
         private readonly static Dictionary<string, TagToken> TagDictonary = new()
-    {
-        { "######", new TagToken("######", "h6",      TagType.Header6, TagPosition.Start)},
-        { "#####" , new TagToken("#####" , "h5",      TagType.Header5, TagPosition.Start)},
-        { "####"  , new TagToken("####"  , "h4",      TagType.Header4, TagPosition.Start)},
-        { "###"   , new TagToken("###"   , "h3",      TagType.Header3, TagPosition.Start)},
-        { "##"    , new TagToken("##"    , "h2",      TagType.Header2, TagPosition.Start)},
-        { "#"     , new TagToken("#"     , "h1",      TagType.Header1, TagPosition.Start)},
-        { "__"    , new TagToken("__"    , "strong",  TagType.Bold,    TagPosition.Start)},
-        { "_"     , new TagToken("_"     , "em",      TagType.Italic,  TagPosition.Start)}
-    };
+        {
+            { "######", new TagToken("######", "h6",      TagType.Header, TagPosition.Start)},
+            { "#####" , new TagToken("#####" , "h5",      TagType.Header, TagPosition.Start)},
+            { "####"  , new TagToken("####"  , "h4",      TagType.Header, TagPosition.Start)},
+            { "###"   , new TagToken("###"   , "h3",      TagType.Header, TagPosition.Start)},
+            { "##"    , new TagToken("##"    , "h2",      TagType.Header, TagPosition.Start)},
+            { "#"     , new TagToken("#"     , "h1",      TagType.Header, TagPosition.Start)},
+            { "__"    , new TagToken("__"    , "strong",  TagType.Bold,   TagPosition.Start)},
+            { "_"     , new TagToken("_"     , "em",      TagType.Italic, TagPosition.Start)}
+        };
 
         public readonly static string[] Tags = { "__", "_" };
 
