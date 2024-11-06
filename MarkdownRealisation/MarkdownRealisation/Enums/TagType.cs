8ffea2c18a436c07b4ed19347746c939950e093f
blessed-one <shakirovdanis05@gmail.com>
2024-11-07 00:08:46 +0300

    Изменил типы тегов

--- MarkdownRealisation/MarkdownRealisation/Enums/TagType.cs
+++ MarkdownRealisation/MarkdownRealisation/Enums/TagType.cs
@@ -4,12 +4,7 @@
     {
         Italic,
         Bold,
-        Header1,
-        Header2,
-        Header3,
-        Header4,
-        Header5,
-        Header6,
+        Header,
         Paragraph
     }
 }
