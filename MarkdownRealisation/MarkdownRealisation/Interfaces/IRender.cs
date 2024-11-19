0ba76f35be0dab2f6d3bbc4e8729d7a10a6738be
blessed-one <shakirovdanis05@gmail.com>
2024-11-19 18:24:39 +0300

    Изменил коментарий в контракте

--- MarkdownRealisation/MarkdownRealisation/Interfaces/IRender.cs
+++ MarkdownRealisation/MarkdownRealisation/Interfaces/IRender.cs
@@ -3,10 +3,10 @@
     public interface IRender
     {
         /// <summary>
-        /// Конвертирует текст с Markdown разметкой в HTML
+        /// Конвертирует текст из одной разметки в другую
         /// </summary>
-        /// <param name="text">Текст с md разметкой</param>
-        /// <returns>Текст с html разметкой</returns>
+        /// <param name="text">Текст с исходной разметкой</param>
+        /// <returns>Текст с изменённой разметкой</returns>
         public string Render(string text);
     }
 }
